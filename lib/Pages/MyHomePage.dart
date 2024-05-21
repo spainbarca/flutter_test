82f7ca861f4b252a0b055358b4ce6552fc50c653
Noah <practicasjaguar@gmail.com>
2024-05-21 02:29:58 -0500

    fondo vacio texto arriba

---
+++ lib/Pages/MyHomePage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pinkAccent,
+      body: Text(
+        'Hola que tal'
+      ),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
